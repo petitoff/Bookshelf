d5b9209a0fe082dd637dbc35af43ec28ed6d9b71
Błażej Domagała <petitoff@outlook.com>
2023-04-07 19:08:48 +0200

    removed unused import

--- src/components/common/LoginForm/LoginForm.tsx
+++ src/components/common/LoginForm/LoginForm.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "./LoginForm.module.css";
 import useLogin from "../../../hooks/useLogin";
-import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const LoginForm = () => {
