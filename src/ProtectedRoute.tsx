abef5bed10d293236386ec5751abd5f57c10281e
Błażej Domagała <petitoff@outlook.com>
2023-04-09 22:28:49 +0200

    Using useCallback and removed unused fields in style

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -1,25 +1,26 @@
-import { Redirect, Route } from "react-router-dom";
+import { useCallback } from "react";
 import { useAppSelector } from "./hooks/hooks";
+import { Redirect, Route } from "react-router-dom";
 
 const ProtectedRoute = ({ children, ...rest }: any) => {
   const user = useAppSelector((state) => state.auth.user);
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        user ? (
-          children
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/login",
-              state: { from: location },
-            }}
-          />
-        )
-      }
-    />
+
+  const renderProtectedRoute = useCallback(
+    ({ location }: any) =>
+      user ? (
+        children
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: location },
+          }}
+        />
+      ),
+    [user, children]
   );
+
+  return <Route {...rest} render={renderProtectedRoute} />;
 };
 
 export default ProtectedRoute;
