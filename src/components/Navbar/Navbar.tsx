db4a9f2deda6876bf7156be076d1dd756a178e07
Błażej Domagała <petitoff@outlook.com>
2023-04-30 13:43:16 +0200

    Refactor code in Navbar component with changed Navbar.scss to module.scss

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { FaSearch, FaBars, FaTimes } from "react-icons/fa";
-import "./Navbar.scss";
-import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
-import { toggleLeftSidebar } from "../../store/slices/sidebarSlice";
 import { Link, useLocation } from "react-router-dom";
 import useBookSearch from "../../hooks/useBookSearch";
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
+import { toggleLeftSidebar } from "../../store/slices/sidebarSlice";
+import styles from "./Navbar.module.scss";
 
-const RouteToExclude = ["/book/:id", "/login", "/signup"];
+const EXCLUDED_ROUTES = ["/book/:id", "/login", "/signup"];
 
-const Navigation = () => {
+const Navbar = () => {
   const [activeItem, setActiveItem] = useState("books");
   const [searchQuery, setSearchQuery] = useState("");
+  const dispatch = useAppDispatch();
+  const location = useLocation();
+  const isSearchBarActive = !EXCLUDED_ROUTES.includes(location.pathname);
   const isLeftSidebarOpen = useAppSelector(
     (state) => state.sidebar.isLeftSidebarOpen
   );
@@ -19,31 +22,28 @@ const Navigation = () => {
   );
   const { setSearchTerm } = useBookSearch();
 
-  const dispatch = useAppDispatch();
-  const location = useLocation();
-  const isSearchBarActive = !RouteToExclude.includes(location.pathname);
-
-  const handleMenuItemClick = (item: any) => {
+  const handleMenuItemClick = (item: string) => {
     setActiveItem(item);
   };
 
-  const handleSearchInputChange = (event: any) => {
-    setSearchQuery(event.target.value);
-    setSearchTerm(event.target.value);
+  const handleSearchInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { value } = event.target;
+    setSearchQuery(value);
+    setSearchTerm(value);
   };
 
-  const handleSearchSubmit = (event: any) => {
+  const handleSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // Handle search submit here
   };
 
   const handleToggleLeftSidebar = () => {
-    // Handle sidebar toggle here
     dispatch(toggleLeftSidebar());
   };
 
-  // if press esc key, close the sidebar
-  const handleKeyDown = (event: any) => {
+  const handleKeyDown = (event: KeyboardEvent) => {
     if (event.keyCode === 27) {
       dispatch(toggleLeftSidebar());
     }
@@ -55,43 +55,48 @@ const Navigation = () => {
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <nav className={`navigation ${isRightSidebarOpen && "open-right-sidebar"}`}>
-      <ul className="navigation__menu">
-        <li className="open-sidebar" onClick={handleToggleLeftSidebar}>
+    <nav
+      className={`${styles.navigation} ${
+        isRightSidebarOpen && styles["open-right-sidebar"]
+      }`}
+    >
+      <ul className={styles["navigation__menu"]}>
+        <li
+          className={styles["open-sidebar"]}
+          onClick={handleToggleLeftSidebar}
+        >
           {isLeftSidebarOpen ? <FaTimes /> : <FaBars />}
         </li>
         <li
-          className={`navigation__menu-item ${
-            activeItem === "books" ? "active" : ""
+          className={`${styles["navigation__menu-item"]} ${
+            activeItem === "books" && styles.active
           }`}
           onClick={() => handleMenuItemClick("books")}
         >
-          <Link to="/books" className="disable-decorations ">
+          <Link to="/books" className={styles["disable-decorations"]}>
             Books
           </Link>
         </li>
         {/* <li
-          className={`navigation__menu-item ${
-            activeItem === "audiobooks" ? "active" : ""
-          }`}
+          className={`${styles["navigation__menu-item"]} ${activeItem === "audiobooks" && styles.active}`}
           onClick={() => handleMenuItemClick("audiobooks")}
         >
           Audiobooks
         </li>
         <li
-          className={`navigation__menu-item ${
-            activeItem === "podcasts" ? "active" : ""
-          }`}
+          className={`${styles["navigation__menu-item"]} ${activeItem === "podcasts" && styles.active}`}
           onClick={() => handleMenuItemClick("podcasts")}
         >
           Podcasts
         </li> */}
       </ul>
-      <form onSubmit={handleSearchSubmit} className="navigation__search">
+      <form
+        onSubmit={handleSearchSubmit}
+        className={styles["navigation__search"]}
+      >
         {isSearchBarActive && (
           <>
             <input
@@ -99,10 +104,12 @@ const Navigation = () => {
               placeholder="Genre, author or book name"
               value={searchQuery}
               onChange={handleSearchInputChange}
-              className="navigation__search-input"
-              // autoComplete="off"
+              className={styles["navigation__search-input"]}
             />
-            <button type="submit" className="navigation__search-button">
+            <button
+              type="submit"
+              className={styles["navigation__search-button"]}
+            >
               <FaSearch color="#aaa" size={15} />
             </button>
           </>
@@ -112,4 +119,4 @@ const Navigation = () => {
   );
 };
 
-export default Navigation;
+export default Navbar;
