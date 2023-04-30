cc8f8d7547fd6936febd4e1ce6d34ee432a6eeb4
Błażej Domagała <petitoff@outlook.com>
2023-04-30 13:52:17 +0200

    fix: eslint-disable-next-line in Navbar useEffect dependencies

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -55,6 +55,8 @@ const Navbar = () => {
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
