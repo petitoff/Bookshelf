6c1f31f28ad607000236209c9d5a42b038c36dbc
Błażej Domagała <petitoff@outlook.com>
2023-05-05 18:57:18 +0200

    feat: added loading spin indicator while loading books

--- src/hooks/useBooks.ts
+++ src/hooks/useBooks.ts
@@ -1,18 +1,22 @@
 import { db } from "../firebase/config";
 import { collection, onSnapshot, query } from "firebase/firestore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Book } from "../types/Book";
 import { useAppDispatch, useAppSelector } from "./hooks";
 import { setBooks } from "../store/slices/bookSlice";
 
 export function useBooks() {
   const user = useAppSelector((state) => state.auth.user);
+  const [fetchingStatus, setFetchingStatus] = useState<
+    "idle" | "loading" | "succeeded" | "failed"
+  >("idle");
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     let unsubscribe: () => void;
 
     const fetchBooks = async () => {
+      setFetchingStatus("loading");
       const booksCollection = query(collection(db, "books"));
       unsubscribe = onSnapshot(booksCollection, (snapshot) => {
         const bookList = snapshot.docs.map((doc) => {
@@ -26,6 +30,7 @@ export function useBooks() {
         });
 
         dispatch(setBooks(bookList));
+        setFetchingStatus("succeeded");
       });
     };
 
@@ -40,5 +45,5 @@ export function useBooks() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user?.UID]);
 
-  return {};
+  return { fetchingStatus };
 }
--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -5,35 +5,41 @@ import { useAppSelector } from "../hooks/hooks";
 import { useBooks } from "../hooks/useBooks";
 import useUserData from "../hooks/useUserData";
 import styles from "./Pages.module.scss";
+import mainStyles from "../styles/MainStyles.module.scss";
 
 const Books = () => {
-  useBooks();
+  const { fetchingStatus } = useBooks();
   useUserData();
 
-  const booksSearch = useAppSelector((state) => state.books.booksSearch);
-  const books = useAppSelector((state) => state.books.books);
+  const { books, booksSearch } = useAppSelector((state) => state.books);
 
   const isSearchResults = booksSearch && booksSearch.length > 0;
 
   return (
     <div className={styles.books}>
-      <div className={styles.booksRightContainer}>
-        <BookInfoRightSidebar />
-      </div>
+      <BookInfoRightSidebar />
       <div className={styles.booksLeftContainer}>
-        {isSearchResults ? (
-          <div className={styles.booksContainer}>
-            <BookSection
-              titleOfSection={"Search Results"}
-              books={booksSearch}
-            />
+        {fetchingStatus === "loading" ? (
+          <div className={mainStyles.loadingContainer}>
+            <div className={mainStyles.loading} />
           </div>
         ) : (
           <>
-            <ForYouSection />
-            <div className={styles.booksContainer}>
-              <BookSection titleOfSection={"Popular Books"} books={books} />
-            </div>
+            {isSearchResults ? (
+              <div className={styles.booksContainer}>
+                <BookSection
+                  titleOfSection="Search Results"
+                  books={booksSearch}
+                />
+              </div>
+            ) : (
+              <>
+                <ForYouSection />
+                <div className={styles.booksContainer}>
+                  <BookSection titleOfSection="Popular Books" books={books} />
+                </div>
+              </>
+            )}
           </>
         )}
       </div>
