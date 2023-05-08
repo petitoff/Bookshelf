a309234acd3a704e08f8b7618f3d28f9297e7cfe
Błażej Domagała <petitoff@outlook.com>
2023-05-08 16:40:03 +0200

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import {BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
-import {ToastContainer} from "react-toastify";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import {useAppSelector} from "./hooks/hooks";
+import { useAppSelector } from "./hooks/hooks";
 import "./App.css";
 
 // components
@@ -19,59 +19,59 @@ import UserReadingListView from "./pages/UserReadingListView";
 import WelcomeView from "./pages/WelcomeView";
 
 function App() {
-    const user = useAppSelector((state) => state.auth.user);
-    const userIsAuthenticated = Boolean(user);
-    const isNewUser = user && !user.username;
+  const user = useAppSelector((state) => state.auth.user);
+  const userIsAuthenticated = Boolean(user);
+  const isNewUser = user && !user.username;
 
-    const renderRouteWithRedirect = (Component: React.ComponentType) => {
-        if (isNewUser) {
-            return <Redirect to="/welcome"/>;
-        } else {
-            return <Component/>;
-        }
-    };
+  const renderRouteWithRedirect = (Component: React.ComponentType) => {
+    if (isNewUser) {
+      return <Redirect to="/welcome" />;
+    } else {
+      return <Component />;
+    }
+  };
 
-    return (
-        <div>
-            <BrowserRouter>
-                {/*<NewUserRedirect user={user} isNewUser={isNewUser}/>*/}
+  return (
+    <div>
+      <BrowserRouter>
+        {/*<NewUserRedirect user={user} isNewUser={isNewUser}/>*/}
 
-                <Navbar/>
-                <Sidebar/>
-                <ToastContainer position="top-center"/>
+        <Navbar />
+        <Sidebar />
+        <ToastContainer position="top-center" />
 
-                <Switch>
-                    <ProtectedRoute exact path="/welcome">
-                        <WelcomeView isNewUser={isNewUser}/>
-                    </ProtectedRoute>
+        <Switch>
+          <ProtectedRoute exact path="/welcome">
+            <WelcomeView isNewUser={isNewUser} />
+          </ProtectedRoute>
 
-                    <Route exact path="/">
-                        <Redirect to="/books"/>
-                    </Route>
-                    <Route exact path="/books">
-                        {renderRouteWithRedirect(Books)}
-                    </Route>
-                    <Route exact path="/book/:id">
-                        {renderRouteWithRedirect(Book)}
-                    </Route>
-                    <Route exact path="/login">
-                        {userIsAuthenticated ? <Redirect to="/books"/> : <Login/>}
-                    </Route>
-                    <Route exact path="/signup">
-                        {userIsAuthenticated ? <Redirect to="/books"/> : <Signup/>}
-                    </Route>
-                    <Route exact path="/:username/reading-list">
-                        {renderRouteWithRedirect(UserReadingListView)}
-                    </Route>
-                    <ProtectedRoute exact path="/settings">
-                        {renderRouteWithRedirect(Settings)}
-                    </ProtectedRoute>
+          <Route exact path="/">
+            <Redirect to="/books" />
+          </Route>
+          <Route exact path="/books">
+            {renderRouteWithRedirect(Books)}
+          </Route>
+          <Route exact path="/book/:id">
+            {renderRouteWithRedirect(Book)}
+          </Route>
+          <Route exact path="/login">
+            {userIsAuthenticated ? <Redirect to="/books" /> : <Login />}
+          </Route>
+          <Route exact path="/signup">
+            {userIsAuthenticated ? <Redirect to="/books" /> : <Signup />}
+          </Route>
+          <Route exact path="/:username/reading-list">
+            {renderRouteWithRedirect(UserReadingListView)}
+          </Route>
+          <ProtectedRoute exact path="/settings">
+            {renderRouteWithRedirect(Settings)}
+          </ProtectedRoute>
 
-                    {/*<Route path="/*" component={ErrorPage} />*/}
-                </Switch>
-            </BrowserRouter>
-        </div>
-    );
+          {/*<Route path="/*" component={ErrorPage} />*/}
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
