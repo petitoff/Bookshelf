4ec35fead8b6938c13715d6fd3f8352e1eeff744
Błażej Domagała <petitoff@outlook.com>
2023-07-19 19:15:32 +0200

    display only book of category in For you section that user like

--- src/components/ForYouSection/ForYouSection.tsx
+++ src/components/ForYouSection/ForYouSection.tsx
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import { useAppSelector } from "../../hooks/hooks";
 import WideBookCard from "../common/WideBookCard/WideBookCard";
 import "./ForYouSection.css";
-import { Book } from "../../types/Book";
+import { Book, BookCategory } from "../../types/Book";
 
 const ForYouSection = () => {
+  const userListOfFavoriteBookCategories = useAppSelector(
+    (state) => state.auth.user?.favouriteCategories
+  );
   const books = useAppSelector((state) => state.books.books);
   const [randomBooks, setRandomBooks] = useState<Book[]>([]);
 
@@ -14,8 +17,23 @@ const ForYouSection = () => {
   };
 
   useEffect(() => {
-    setRandomBooks(getRandomBooks(books, 2));
-  }, [books]);
+    if (!userListOfFavoriteBookCategories) return;
+
+    const getBooksByCategories = (categories: BookCategory[]) => {
+      const booksByCategories: Book[] = [];
+      categories.forEach((category) => {
+        const booksByCategory = books.filter(
+          (book) => book.category === category
+        );
+        booksByCategories.push(...booksByCategory);
+      });
+      return booksByCategories;
+    };
+
+    setRandomBooks(
+      getRandomBooks(getBooksByCategories(userListOfFavoriteBookCategories), 2)
+    );
+  }, [books, userListOfFavoriteBookCategories]);
 
   return (
     <section className="foryou-section">
